07d74e34f81271530d5e213a030a3f4568eb05db
alexpetre27 <147337105+alexpetre27@users.noreply.github.com>
2024-05-08 22:30:51 +0300

    Initial commit

---
+++ pamgirevreact/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import Sidenav from './components/Sidenav'
+import MainPage from './components/MainPage'
+import PhotoGallery from './components/PhotoGallery'
+import VideoGallery from './components/VideoGallery'
+import Offers from './components/Offers'
+import Contact from './components/Contact'
+
+export default function App(){
+  return(
+    <div>
+      <Sidenav/>
+      <MainPage/>
+      <PhotoGallery/>
+      <VideoGallery/>
+      <Offers/>
+      <Contact/>
+    </div>
+  );
+}
