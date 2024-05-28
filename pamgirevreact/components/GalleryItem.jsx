ae8580972ece1f3f860b0d1b0aa20eee5d72883f
alexpetre27 <147337105+alexpetre27@users.noreply.github.com>
2024-05-28 09:48:40 +0300

    28 mai

--- pamgirevreact/components/GalleryItem.jsx
+++ pamgirevreact/components/GalleryItem.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React from 'react';
 
-export defaul function GalleryItem(){
-    return(
-        <>
-            <ul>
-                <li>
-                     <a href=""></a>
-                </li>
-            </ul> 
-        </>
-    );
+function GalleryItem({ source, figcaption }) {
+  return (
+    <>
+      <div className="relative overflow-hidden">
+        <div className="w-full block aspect-square object-cover transition-transform">
+          <img src={source} alt={figcaption} />
+          <div className="absolute inset-0 bg-black bg-opacity-50 transition-opacity opacity-0 hover:opacity-100 flex justify-center items-center">
+            <h3 className="text-white text-center text-xl">{figcaption}</h3>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
+
+export default GalleryItem;
--- pamgirevreact/components/PhotoGallery.jsx
+++ pamgirevreact/components/PhotoGallery.jsx
@@ -1,13 +1,44 @@
-import React from "react";
+import {React} from "react";
+import Images from '../images/Images'
+import GalleryItem from "./GalleryItem"; 
 
 export default function PhotoGallery(){
-    return(
-        <div id="photogallery">
-            <div className="w-full h-screen bg-gslate-200 bg-white/90" >
-                <h1 className="text-center p-8 text-2xl font-bold text-slate-800 w-full" >Galerie Foto</h1>
-            </div>
-            <GalleryItem/>
-        </div>
-    );
-    
+  const photo=[
+    {  
+    source:Images.image1,
+    figcaption:'asdfasd1',
+    },
+    {  
+    source:Images.image2,
+    figcaption:'asdfasd2',
+    },
+    {  
+    source:Images.image3,
+    figcaption:'asdfasd3',
+    },
+    {  
+    source:Images.image4,
+    figcaption:'asdfasd4',
+    },
+    {  
+    source:Images.image5,
+    figcaption:'asdfasd5',
+    },
+    {  
+    source:Images.image6,
+    figcaption:'asdfasd6',
+    },
+   
+  ];
+  return(
+    <div className="m-auto p-4 min-h-[100vh] flex justify-center items-center">
+      <ul className="list-none m-0 p-0 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 max-w-[100%] w-[70rem]"> 
+        {photo.map((photo, index) => (
+          <li key={index}>
+            <GalleryItem src={photo.source} figcaption={photo.figcaption} className="m-0 relative overflow-hidden"/>
+          </li>
+        ))}
+      </ul>
+    </div>   
+  );
 }
